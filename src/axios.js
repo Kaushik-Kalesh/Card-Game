fd1537d9761ea80b398da220448486aee8828f16
Kaushik <67593056+Kaushik-Kalesh@users.noreply.github.com>
2021-08-22 13:06:17 +0530

    Delete axios.js

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://5000-purple-salmon-uy3kza3n.ws-us14.gitpod.io/",
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
